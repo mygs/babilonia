67f774ec7fb3a675e1a0256c49a03421d17a72fc
mygos <saito.milton@gmail.com>
2018-02-13 07:21:41 -0200

    Temperature chart test

--- server/app/static/js/chart.js
+++ server/app/static/js/chart.js
@@ -1,37 +1,49 @@
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
+/* check http://www.chartjs.org/docs/latest/ */
+
+$('#chartNodeModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) ;// Button that triggered the modal
+  var id = button.data('id');
+	var name = button.data('name');
+	var modal = $(this);
+	modal.find('.modal-title').text('Charts of ' + name);
+
+	var timeseries = {
+		"temperature":{
+			"label": ["JAN", "FEV", "MAR", "ABR", "MAI","JUN","JUL","AGO","SET","OUT","NOV","DEZ"],
+			"data": [	{x:1,y:33},{x:2,y:34},{x:3,y:29},{x:4,y:27},{x:5,y:28},{x:6,y:25},
+								{x:7,y:22},{x:8,y:21},{x:9,y:21},{x:10,y:23},{x:11,y:25},{x:12,y:29}],
+		}
+	}
+
+	var myChart = new Chart($('#temperatureChart'), {
+	    type: 'line',
+	    data: {
+	        labels: timeseries["temperature"]["label"],
+	        datasets: [{
+	            label: 'Temperature',
+	            data:timeseries["temperature"]["data"],
+							fill:false,
+							borderColor:"rgb(75, 192, 192)",
+							lineTension:0.5,
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {}
+	});
+	/*
+	$.ajax({
+		url: '/timseries',
+		type: 'POST',
+		data: {"id": id },
+		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+		success: function (response) {
+				var resp = JSON.parse(response);
+
+				$("#ID").val(id);
+				$("#NAME").val(resp.NAME);
+		},
+		error: function (response) {
+		}
+	});
+*/
 });
