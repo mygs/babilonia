67292da8670801c6217d52d4aa66a131868c96fd
mygos <saito.milton@gmail.com>
2018-09-24 20:02:33 -0300

    data analysis

--- research/data-handler.R
+++
@@ -1,5 +0,0 @@
-# Handling data with R: just a reference
-# reading csv file ...
-# HEADER: NODE,TIMESTAMP,VALUE,SLEEP_TIME_MOISTURE,MOISTURE_NSAMPLE,MOISTURE_NSAMPLE_TIME
-MOISTURE <- read.csv("./moisture-shortcircuit.csv", head = TRUE, sep=",");
-head(MOISTURE$SLEEP_TIME_MOISTURE)
--- research/moisture-sensor-var.R
+++ research/moisture-sensor-var.R
@@ -1,13 +1,19 @@
 library(dplyr)
-#suppressPackageStartupMessages(library(dplyr))
+setwd("/data/github/babilonia/research")
+suppressPackageStartupMessages(library(dplyr))
+
 analyze <- function(filename){
+  print('===========================================================================')
+  print(toupper(filename))
   moisture_data <- read.csv(filename, head = TRUE, sep=",");
   moisture_data <- group_by(moisture_data, SLEEP_TIME_MOISTURE, MOISTURE_NSAMPLE, MOISTURE_NSAMPLE_TIME)
   summarise(moisture_data, count = n(), mean = mean(VALUE), stddev = sd(VALUE))
 }
+
 analyze("./moisture-nickelchromeplate-soildry.csv")
 analyze("./moisture-nickelchromeplate-soilwet.csv")
 analyze("./moisture-nickelchromeplate-supersoilwet.csv")
+analyze("./moisture-nickelchromeplate-supersoilwet2.csv")
 analyze("./moisture-nickelchromeplate-water.csv")
 analyze("./moisture-shortcircuit.csv")
 analyze("./moisture-soilwet.csv")
