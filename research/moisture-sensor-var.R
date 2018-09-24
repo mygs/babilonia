638c0a1c1cf23bc29e8e301d13df4f719d4012ca
mygs <saito.milton@gmail.com>
2018-09-23 21:41:07 -0300

    more stats info

--- research/moisture-sensor-var.R
+++ research/moisture-sensor-var.R
@@ -1,6 +1,13 @@
 library(dplyr)
-
-MOISTURE <- read.csv("./moisture-water.csv", head = TRUE, sep=",");
-#str(MOISTURE)
-setups <- group_by(MOISTURE, SLEEP_TIME_MOISTURE, MOISTURE_NSAMPLE, MOISTURE_NSAMPLE_TIME)
-summarise(setups, stddev = sd(VALUE))
+#suppressPackageStartupMessages(library(dplyr))
+#moisture-nickelchromeplate-soilwet.csv
+#moisture-nickelchromeplate-supersoilwet.csv
+#moisture-nickelchromeplate-water.csv
+#moisture-sensor-var.R
+#moisture-shortcircuit.csv
+#moisture-soilwet.csv
+#moisture-supersoilwet.csv
+#moisture-water.csv
+moisture_nickelchromeplate_soilwet <- read.csv("./moisture-nickelchromeplate-soilwet.csv", head = TRUE, sep=",");
+moisture_nickelchromeplate_soilwet <- group_by(moisture_nickelchromeplate_soilwet, SLEEP_TIME_MOISTURE, MOISTURE_NSAMPLE, MOISTURE_NSAMPLE_TIME)
+summarise(moisture_nickelchromeplate_soilwet, count = n(), mean = mean(VALUE), stddev = sd(VALUE))
