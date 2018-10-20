a9c345b90119cd60aedb9cff27c3b524e70c2d9f
mygos <saito.milton@gmail.com>
2018-10-20 09:23:53 -0300

    log

--- server/irrigation.sh
+++ server/irrigation.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 NODEID=15566415
-TIME=60000000
+TIME=10000000
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
 CONFIG_FILE='./app/config.json'
 MQTT_HOST=`cat  $CONFIG_FILE | jq -r .mqtt.broker`
 PARM="id:$NODEID;sop:$TIME"
 mosquitto_pub -h $MQTT_HOST -t "/cfg" -m $PARM
+echo "$DATA - $PARM"
