c13ee4ddad03f32be6f30c0a5dc51f9037019a6b
mygos <saito.milton@gmail.com>
2018-10-20 09:50:14 -0300

    oasis 1x

--- server/irrigation.sh
+++ server/irrigation.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#38 09 * * * /babilonia/server/irrigation.sh > /babilonia/server/log/irrigation.log 2>&1 &
-NODEID=15566415
-TIME=10000000
+#sudo crontab -e
+#10 17 * * * /babilonia/server/irrigation.sh > /babilonia/server/log/irrigation.log 2>&1 &
+NODEID=15566415 #OASIS 1X - outdoor
+TIME=300000000 # 5min
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
 CONFIG_FILE='/babilonia/server/app/config.json'
-MQTT_HOST=`cat  $CONFIG_FILE | jq -r .mqtt.broker`
+MQTT_HOST=`cat $CONFIG_FILE | jq -r .mqtt.broker`
 PARM="id:$NODEID;sop:$TIME"
 mosquitto_pub -h $MQTT_HOST -t "/cfg" -m $PARM
 echo "$DATE - $PARM"
