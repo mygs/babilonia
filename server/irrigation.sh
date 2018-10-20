3b22fd0fe712678da4675388d5d7ee6feaf7cd04
mygos <saito.milton@gmail.com>
2018-10-20 09:41:18 -0300

    full path

--- server/irrigation.sh
+++ server/irrigation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#28 09 * * * nohup /babilonia/server/irrigation.sh > /babilonia/server/log/irrigation.log 2>&1 &
+#38 09 * * * /babilonia/server/irrigation.sh > /babilonia/server/log/irrigation.log 2>&1 &
 NODEID=15566415
 TIME=10000000
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
-CONFIG_FILE='./app/config.json'
+CONFIG_FILE='/babilonia/server/app/config.json'
 MQTT_HOST=`cat  $CONFIG_FILE | jq -r .mqtt.broker`
 PARM="id:$NODEID;sop:$TIME"
 mosquitto_pub -h $MQTT_HOST -t "/cfg" -m $PARM
