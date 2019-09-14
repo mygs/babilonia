42d59049ec707e384af3d761a122bd63b0200a08
mygs <saito.milton@gmail.com>
2019-09-14 18:08:11 -0300

    progress bar

--- server/app/static/js/firmware.js
+++ server/app/static/js/firmware.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
+
+  $('#firmware').on('init.dt', function() {
+   $('.firware-build-btn')
+     .attr('data-toggle', 'collapse')
+     .attr('data-target', '#updating-node');
+   });
+
   var table = $('#firmware').DataTable({
     "bLengthChange": false,
     "info": false,
     "bPaginate": false, //hide pagination control
     "dom": 'Bfrtip',
     "buttons": [{
-          text: '<i class="fa fa-plus" aria-hidden="true"></i>',
+          text: '<i class="fa fa-microchip" aria-hidden="true"></i>',
+          className: 'firware-build-btn',
           titleAttr: 'Build Firmware',
           action: function(e, dt, node, config) {
               console.log("build firware");
           }
         }]
+
   });
-}
+});
