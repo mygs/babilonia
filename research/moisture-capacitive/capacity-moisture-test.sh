3c6d75b6dbdc88a49213227d9572719cf6e951f1
mygs <saito.milton@gmail.com>
2019-10-20 17:41:02 -0300

    test script

--- research/moisture-capacitive/capacity-moisture-test.sh
+++ research/moisture-capacitive/capacity-moisture-test.sh
@@ -8,9 +8,9 @@ TEST_DURATION_SECS=5
 #==== Do not touch below here
 TOTAL_SAMPLES=$((TEST_DURATION_SECS/COLLECT_INTERVAL_SECS))
 
-SERVER_BAB=192.168.1.70
+SERVER=192.168.1.70
 if [ `hostname` = "babilonia" ]; then
-  SERVER_BAB=192.168.2.1
+  SERVER=192.168.2.1
 fi
 ##### MQTT
 MQTT_TOPIC="/oasis-inbound"
@@ -19,16 +19,19 @@ MQTT_MSG="{\"NODE_ID\": \"$ID\", \"MESSAGE_ID\": \"$MESSAGE_ID\",\"STATUS\": [\"
 ##### DATABASE
 CONFIG_FILE='../../server/app/config.json'
 DB_PWD=`cat  $CONFIG_FILE | jq -r .SECRET_KEY`
-INITIAL_COUNT=`mysql -h$SERVER_BAB -ubabilonia -p$DB_PWD -s -N -e "SELECT count(*) FROM farmland.OASIS_DATA WHERE NODE_ID='$MQTT_NODE_ID'" 2>/dev/null`
+
+echo '########################################'
+START=`date +%s`
+OFFSET_COUNT=`mysql -h$SERVER -ubabilonia -p$DB_PWD -s -N -e "SELECT count(*) FROM farmland.OASIS_DATA WHERE NODE_ID='$MQTT_NODE_ID'" 2>/dev/null`
 echo 'Total samples: '$TOTAL_SAMPLES
-echo 'Initial count: '$INITIAL_COUNT
-LAST_SAMPLE=$((TOTAL_SAMPLES+INITIAL_COUNT))
-echo 'Last count: '$LAST_SAMPLE
-
-while : ; do
-  #COUNT=`$MYSQL_CMD`
-  break
+LAST_SAMPLE=$((TOTAL_SAMPLES+OFFSET_COUNT))
+COUNT=$OFFSET_COUNT
+while [ "$COUNT" -le "$LAST_SAMPLE" ] ; do
+  mosquitto_pub -h $SERVER -t $MQTT_TOPIC -m $MQTT_MSG
+  sleep $COLLECT_INTERVAL_SECS
+  COUNT=`mysql -h$SERVER -ubabilonia -p$DB_PWD -s -N -e "SELECT count(*) FROM farmland.OASIS_DATA WHERE NODE_ID='$MQTT_NODE_ID'" 2>/dev/null`
+  echo $((COUNT-OFFSET_COUNT))' of '$TOTAL_SAMPLES
 done
-echo $MSG
-
-#mosquitto_pub -h $SERVER_BAB -t $MQTT_TOPIC -m $MQTT_MSG
+END=`date +%s`
+echo "Execution time was `expr $end - $start` seconds."
+echo '########################################'
