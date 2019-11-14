0ae8a7e48448d4a7df7787988413454093ad2a0d
mygs <saito.milton@gmail.com>
2019-11-13 22:28:36 -0300

    status

--- server/app/server_status.py
+++ server/app/server_status.py
@@ -1,39 +1,36 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import sys,os,time,posix,glob
+import sys,os,glob
 
 def cputemp():
   f = open("/sys/class/thermal/thermal_zone0/temp")
   CPUTemp = f.read()
   f.close()
-  StringToOutput= str(int(CPUTemp)/1000.0)
-  return StringToOutput
+  return "%dC" % (int(CPUTemp)/1000.0)
 
-
-
-def proc_meminfo():
+def meminfo():
   items = {}
   for l in open('/proc/meminfo').readlines():
     a = l.split()
     items[a[0]] = int(a[1])
-  # print items['MemTotal:'], items['MemFree:'], items['SwapTotal:'], items['SwapFree:']
-  return items
+  return "%d%%" % (100-100.*items['MemFree:']/(items['MemTotal:'] or 1))
 
-loadav = float(open("/proc/loadavg").read().split()[1])
-processes = len(glob.glob('/proc/[0-9]*'))
-statfs = os.statvfs('/')
-rootperc = 100-100.*statfs.f_bavail/statfs.f_blocks
-temp = cputemp()
-meminfo = proc_meminfo()
-memperc = "%d%%" % (100-100.*meminfo['MemFree:']/(meminfo['MemTotal:'] or 1))
-swapperc = "%d%%" % (100-100.*meminfo['SwapFree:']/(meminfo['SwapTotal:'] or 1))
+def disk():
+  items = {}
+  statfs = os.statvfs('/')
+  return "%d%%" % (100-100.*statfs.f_bavail/statfs.f_blocks)
+
+def processes():
+  return len(glob.glob('/proc/[0-9]*'))
+
+def system_load():
+  return float(open("/proc/loadavg").read().split()[1])
 
 
-print ("  Temperature: ",temp)
-print ("  System load: ",loadav)
-print ("  Disk: ",rootperc)
-print ("  Processes: ", processes)
-print ("  Memory usage: ", memperc)
-print ("  Swap usage: ", swapperc)
+print ("  Temperature: ",cputemp())
+print ("  System load: ",system_load())
+print ("  Disk: ",disk())
+print ("  Processes: ", processes())
+print ("  Memory usage: ", meminfo())
 
 sys.exit(0)
