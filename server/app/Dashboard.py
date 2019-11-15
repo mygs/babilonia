7e609ff9c36a235a7f71cccd7832236331810d18
mygs <saito.milton@gmail.com>
2019-11-14 23:56:00 -0300

    dashboard fix

--- server/app/Dashboard.py
+++ server/app/Dashboard.py
@@ -6,7 +6,7 @@ class Dashboard:
     def __init__(self, cfg):
         self.cfg = cfg
 
-    def weather_currently():
+    def weather_currently(self):
         weather_key = self.cfg["WEATHER_KEY"]
         lat = self.cfg["LATITUDE"]
         long = self.cfg["LONGITUDE"]
@@ -16,26 +16,26 @@ class Dashboard:
         logger.debug("[weather] %s", data)
         return data['currently']
 
-    def cpu_temperature():
+    def cpu_temperature(self):
       f = open("/sys/class/thermal/thermal_zone0/temp")
       CPUTemp = f.read()
       f.close()
       return "%d°C" % (int(CPUTemp)/1000.0)
 
-    def memory_usage():
+    def memory_usage(self):
       items = {}
       for l in open('/proc/meminfo').readlines():
         a = l.split()
         items[a[0]] = int(a[1])
       return "%d%%" % (100-100.*items['MemAvailable:']/(items['MemTotal:'] or 1))
 
-    def disk():
+    def disk(self):
       items = {}
       statfs = os.statvfs('/')
       return "%d%%" % (100-100.*statfs.f_bavail/statfs.f_blocks)
 
-    def processes():
+    def processes(self):
       return len(glob.glob('/proc/[0-9]*'))
 
-    def system_load():
+    def system_load(self):
       return float(open("/proc/loadavg").read().split()[1])
