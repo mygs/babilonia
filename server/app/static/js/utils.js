a2cfdad179bad19d3d7d2f9c3efc8ad88168749a
mygs <saito.milton@gmail.com>
2020-01-06 20:33:48 -0300

    irrigation feedback

--- server/app/static/js/utils.js
+++ server/app/static/js/utils.js
@@ -11,6 +11,7 @@ $('#updateNodeModal').on('show.bs.modal', function(event) {
   var button = $(event.relatedTarget); // Button that triggered the modal
   var id = button.data('id'); // Extract info from data-* attributes
   var modal = $(this);
+  $('.panel-collapse').removeClass('in');
   $.ajax({
     url: '/configuration',
     type: 'POST',
