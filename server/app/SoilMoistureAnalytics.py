2fd537dc31787f24438ce462ce3d4881c7a541f7
musashi <saito.milton@gmail.com>
2020-01-17 19:35:36 -0300

    NOSOIL status

--- server/app/SoilMoistureAnalytics.py
+++ server/app/SoilMoistureAnalytics.py
@@ -5,18 +5,19 @@ from Models import *
 import logging
 
 class SoilMoistureAnalytics:
-
+    #default values
     MUX_PORT_THRESHOLD_IDLE=50
     MUX_PORT_THRESHOLD_DRY=600
+    MUX_PORT_THRESHOLD_NOSOIL=680
 
     def __init__(self, logger):
         self.logger = logger
 
     def status(self, node_id, port, level):
-        #analytics = DB.session.query(OasisAnalytic).one()
-        #self.logger.debug("[SoilMoistureAnalytics] %s", analytics.data())
+        analytics = DB.session.query(OasisAnalytic).first()
+        self.logger.debug("[SoilMoistureAnalytics] %s", analytics.data())
 
-        if level < self.MUX_PORT_THRESHOLD_IDLE:
+        if level < self.MUX_PORT_THRESHOLD_IDLE or level > self.MUX_PORT_THRESHOLD_NOSOIL:
             return "moisture-disable"
         else:
             #TODO: put some brain in here
@@ -24,4 +25,5 @@ class SoilMoistureAnalytics:
 
     def param(self):
         return {"MUX_PORT_THRESHOLD_IDLE":self.MUX_PORT_THRESHOLD_IDLE,
-                "MUX_PORT_THRESHOLD_DRY":self.MUX_PORT_THRESHOLD_DRY}
+                "MUX_PORT_THRESHOLD_DRY":self.MUX_PORT_THRESHOLD_DRY,
+                "MUX_PORT_THRESHOLD_NOSOIL":self.MUX_PORT_THRESHOLD_NOSOIL}
