fd98fb9cb5908737f7c274e05a3942b41a98515f
mygs <saito.milton@gmail.com>
2020-01-19 08:51:39 -0300

    more moisture status color

--- server/app/SoilMoistureAnalytics.py
+++ server/app/SoilMoistureAnalytics.py
@@ -6,7 +6,7 @@ import logging
 
 class SoilMoistureAnalytics:
     #default values
-    MUX_PORT_THRESHOLD_IDLE=50
+    MUX_PORT_THRESHOLD_OFFLINE=50
     MUX_PORT_THRESHOLD_DRY=600
     MUX_PORT_THRESHOLD_NOSOIL=680
 
@@ -17,13 +17,15 @@ class SoilMoistureAnalytics:
         analytics = DB.session.query(OasisAnalytic).first()
         self.logger.debug("[SoilMoistureAnalytics] %s", analytics.data())
 
-        if level < self.MUX_PORT_THRESHOLD_IDLE or level > self.MUX_PORT_THRESHOLD_NOSOIL:
-            return "moisture-disable"
+        if level < self.MUX_PORT_THRESHOLD_OFFLINE:
+            return "moisture-offline"
+        elif level > self.MUX_PORT_THRESHOLD_NOSOIL:
+            return "moisture-nosoil"
         else:
             #TODO: put some brain in here
             return "moisture-wet"
 
     def param(self):
-        return {"MUX_PORT_THRESHOLD_IDLE":self.MUX_PORT_THRESHOLD_IDLE,
+        return {"MUX_PORT_THRESHOLD_OFFLINE":self.MUX_PORT_THRESHOLD_OFFLINE,
                 "MUX_PORT_THRESHOLD_DRY":self.MUX_PORT_THRESHOLD_DRY,
                 "MUX_PORT_THRESHOLD_NOSOIL":self.MUX_PORT_THRESHOLD_NOSOIL}
