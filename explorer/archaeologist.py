8190966382cdcac84b54c5a30241aaa4da67971e
musashi <saito.milton@gmail.com>
2020-05-09 17:35:34 -0300

    ceagesp prices

--- explorer/archaeologist.py
+++ explorer/archaeologist.py
@@ -2,9 +2,35 @@
 # -*- coding: utf-8 -*-
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import certifi
-import re
-import json
+import certifi, re, json, requests
+import pandas as pd
+
+
+def ceagesp_dates(url):
+    html = urlopen(url)
+    pattern = re.compile("var Grupos = ")
+    soup = BeautifulSoup(html, 'html.parser')
+    script = soup.find('script',text=pattern)
+    script=str(script)
+    group = script[script.find("var Grupos = "):script.find(";")]
+    group = json.loads(group.replace('var Grupos = ', ''))
+    return group['VERDURAS']
+
+def ceagesp():
+    url = "http://www.ceagesp.gov.br/entrepostos/servicos/cotacoes/#cotacao"
+    data = {"cot_grupo": "VERDURAS", "cot_data": ceagesp_dates(url)[0]}
+    soup = BeautifulSoup(requests.post(url, data).text, 'html.parser')
+
+    table_html = soup.find('table',{"class": "contacao_lista" })
+    if table_html is not None:
+        table_data = [[cell.text.strip() for cell in row("td")]
+                                 for row in table_html("tr")]
+        df = pd.DataFrame(table_data[2:], columns=['Produto','Classificacao','Uni|Peso','Menor','Comun','Maior','Quilo'])
+        print (df.to_json(orient='records'))
+    else:
+        print("None!!!!!")
+
+ceagesp()
 
 def stmarche():
     url = "https://www.marche.com.br/categorias/verduras"
@@ -17,5 +43,4 @@ def stmarche():
     for product in products:
         pjson = json.loads(product['data-json'])
         print(pjson)
-
 #stmarche()
