8074a929e7e9059accde302c48d162867f1db58c
musashi <saito.milton@gmail.com>
2020-05-10 06:06:16 -0300

    crontab + logging for archeologist

--- explorer/archaeologist.py
+++ explorer/archaeologist.py
@@ -1,24 +1,39 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
+#sudo crontab -e
+#00 01 * * * /babilonia/explorer/archeologist.py
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import os, certifi, re, json, requests, datetime
 import pandas as pd
 from Models import *
 from sqlalchemy import create_engine
+import logging
+import logging.config
+
 
 URL={"CEAGESP":"http://www.ceagesp.gov.br/entrepostos/servicos/cotacoes/#cotacao",
     "STMARCHE":"https://www.marche.com.br/categorias/verduras"}
 
 # Create a session
-HOME = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(HOME, '../server/app/config.json'), "r") as config_json_file:
+SERVER_HOME = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(SERVER_HOME, '../server/app/config.json'), "r") as config_json_file:
     cfg = json.load(config_json_file)
 engine = sqlalchemy.create_engine(cfg["SQLALCHEMY_DATABASE_URI"])
 Session = sqlalchemy.orm.sessionmaker()
 Session.configure(bind=engine)
 session = Session()
-
+#logging
+os.chdir(SERVER_HOME) #change directory because of log files
+LOG_DIR=os.path.join(SERVER_HOME, 'log')
+with open(os.path.join(SERVER_HOME, 'logging.json'), "r") as logging_json_file:
+    logging_config = json.load(logging_json_file)
+    if os.path.exists(LOG_DIR) == False:
+        os.makedirs(LOG_DIR)
+    logging.config.dictConfig(logging_config)
+logger = logging.getLogger(__name__)
 
 def ceagesp_dates(url):
     html = urlopen(url)
@@ -32,12 +47,10 @@ def ceagesp_dates(url):
 
 def ceagesp():
     dates =  ceagesp_dates(URL["CEAGESP"])
-    print(dates)
-
+    logger.info("[CEAGESP] available dates: %s",dates)
     for date in dates:
         data = {"cot_grupo": "VERDURAS", "cot_data":date}
-        soup = BeautifulSoup(requests.post(url, data).text, 'html.parser')
-
+        soup = BeautifulSoup(requests.post(URL["CEAGESP"], data).text, 'html.parser')
         table_html = soup.find('table',{"class": "contacao_lista" })
         if table_html is not None:
             table_data = [[cell.text.strip() for cell in row("td")]
@@ -48,9 +61,8 @@ def ceagesp():
             price = Prices(SOURCE="CEAGESP", DATE=date, DATA= df.to_json(orient='records') )
             session.merge(price)
             session.commit()
-            print (price.toJson())
+            logger.debug("[CEAGESP] %s",price.toJson())
 
-#ceagesp()
 
 def stmarche():
     now = datetime.datetime.now()
@@ -62,10 +74,16 @@ def stmarche():
     product_list = []
     for product in products:
         product_list.append(json.loads(product['data-json']))
-
     price = Prices(SOURCE="STMARCHE", DATE=today, DATA= json.dumps(product_list, ensure_ascii=False) )
     session.merge(price)
     session.commit()
-    print (price.toJson())
+    logger.debug("[STMARCHE] %s",price.toJson())
 
-stmarche()
+
+
+now = datetime.datetime.now()
+logger.info("[ARCHEOLOGIST] Starting at %s",now.strftime('%Y-%m-%d %H:%M:%S'))
+#ceagesp()
+#stmarche()
+now = datetime.datetime.now()
+logger.info("[ARCHEOLOGIST] Ending at %s",now.strftime('%Y-%m-%d %H:%M:%S'))
