b3f829a7149f7d2fe3d5e74ec77f11d7ee68c79d
mygos <saito.milton@gmail.com>
2021-02-10 08:51:17 -0300

    organising code

---
+++ server/app/Irrigation.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import time
+import json
+import datetime as dt
+import logging
+import pandas
+import requests
+from Models import DB, OasisTraining, OasisAnalytic
+from sqlalchemy import create_engine, func, and_
+from sqlalchemy.orm import sessionmaker
+
+IRRIGATION_DURATION = 5*60 #seconds
+HEARTBEAT_PERIOD=30 # (seconds) OMG 2 heartbeats
+
+class Irrigation:
+
+    def __init__(self, logger, cfg, mqtt):
+        self.logger = logger
+        self.cfg = cfg
+        self.mqtt = mqtt
+        self.SQLALCHEMY_DATABASE_URI =  cfg["SQLALCHEMY_DATABASE_URI"]
+
+    def run_standard(self):
+
+        url = 'http://%s/water-tank'%(self.cfg["WATER_TANK_SERVER"])
+        headers = {'Content-type': 'application/json'}
+        response = requests.post(url,data=json.dumps({'DIRECTION':'OUT', 'ACTION':True }), headers=headers)
+        self.logger.info("[irrigation]  Water tank server response: %s", response)
+
+        if response.status_code != 200:
+            self.logger.info("[irrigation] Water Tank connection http status code: %s!!!", str(response.status_code))
+
+        self.logger.info("[irrigation] START irrigation")
+
+        ############# get alive nodes #############
+        now = int(time.time())
+        period_for_last_heartbeat = int(now - HEARTBEAT_PERIOD)
+        engine = create_engine(self.SQLALCHEMY_DATABASE_URI)
+
+        nodes = pandas.read_sql_query(
+            """
+            SELECT NODE_ID
+            FROM OASIS_HEARTBEAT
+            WHERE LAST_UPDATE >= {}
+            """.format(period_for_last_heartbeat),
+            engine)
+        node_id = None
+        for index,node in nodes.iterrows():
+            node_id = node['NODE_ID']
+            message = json.dumps({'NODE_ID': str(node_id), 'MESSAGE_ID':"water_sched", 'COMMAND':{"WATER": True}})
+            self.logger.info("[irrigation] %s", message)
+            self.mqtt.publish("/oasis-inbound", message)
+            time.sleep(IRRIGATION_DURATION)
+            message = json.dumps({'NODE_ID': str(node_id), 'MESSAGE_ID':"water_sched", 'COMMAND':{"WATER": False}})
+            self.mqtt.publish("/oasis-inbound", message)
+            self.logger.info("[irrigation] %s", message)
+
+        requests.post(url,data=json.dumps({'DIRECTION':'OUT', 'ACTION':False }), headers=headers)
