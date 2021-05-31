7a33ed0e04f5adc0104417b1cd1433c509f32b9a
mygs <saito.milton@gmail.com>
2021-05-31 20:24:39 -0300

    Update VoiceAssistant.py

--- server/app/VoiceAssistant.py
+++ server/app/VoiceAssistant.py
@@ -40,14 +40,14 @@ W_OASIS, W_DURATION, W_CONFIRMATION, W_BYE = range(4)
 class VoiceAssistant:
 
     def __init__(self, logger, cfg, oasis_props, voice_words):
-        self.logger = logger
-        self.cfg = cfg
+        #self.logger = logger
+        #self.cfg = cfg
         #self.speech_client = speech.SpeechClient()
         #self.storage_client = storage.Client()
-        self.updater = Updater(cfg["TELEGRAM"]["TOKEN"])
-        self.oasis = self.filter_oasis(oasis_props)
-        self.voice_words = voice_words
-        self.user_data_cache = {}
+        #self.updater = Updater(cfg["TELEGRAM"]["TOKEN"])
+        #self.oasis = self.filter_oasis(oasis_props)
+        #self.voice_words = voice_words
+        #self.user_data_cache = {}
 
     def filter_oasis(self, oasis_props):
         result = {}
